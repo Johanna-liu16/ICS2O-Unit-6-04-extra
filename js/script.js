c07d5c254502633fcf3d3816b3520d21760995ec
Johanna-liu16 <Johanna-liu16@users.noreply.github.com>
2022-06-19 23:21:34 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 /**
  * Check servie worker.
  */
- if (navigator.serviceWorker) {
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit-6-04-extra/sw.js", {
     scope: "/ICS2O-Unit-6-04-extra/",
   })
@@ -18,20 +18,21 @@
 /**
  * This function calculates the volume of a sphere.
  */
-window.onload = function() {
+window.onload = function () {
   // this calculates volume of a pyramid
 
   const params = new URLSearchParams(document.location.search)
 
   // input
-  const length = params.get('length')
+  const length = params.get("length")
   console.log(length)
 
   // process
-  const area = (Math.pow(length, 2))
+  const area = Math.pow(length, 2)
   const dimensions = "<ul>\n<li>length = " + length + "</li>\n</ul>"
 
   // output
-  document.getElementById('dimensions').innerHTML = dimensions
-  document.getElementById('area').innerHTML = 'Area is: ' + area.toFixed(2) + ' cm²'
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("area").innerHTML =
+    "Area is: " + area.toFixed(2) + " cm²"
 }
